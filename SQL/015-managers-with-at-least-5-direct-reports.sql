c68b66fbc1105664bcfd281d1da8378c7afce082
prabin Saud <prabinsaud2002@gmail.com>
2026-08-14 21:57:40 +0530

    Add SQL query for LeetCode #570 Managers with at Least 5 Direct Reports

---
+++ SQL/015-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,16 @@
+-- LeetCode #570
+-- Managers with at Least 5 Direct Reports
+
+-- Problem:
+-- Find the names of managers who have at least
+-- five direct reports.
+
+SELECT
+    e1.name
+FROM Employee AS e1
+JOIN Employee AS e2
+    ON e2.managerId = e1.id
+GROUP BY
+    e1.id,
+    e1.name
+HAVING COUNT(e2.managerId) >= 5;
