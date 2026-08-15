cfc14248752455b63b0fdb1ecd095b2ae42c074e
prabin Saud <prabinsaud2002@gmail.com>
2026-08-15 22:13:50 +0530

    Add SQL query for LeetCode #1075 Average Experience Years of Employees by Project

---
+++ SQL/018-project-employees-i.sql
@@ -0,0 +1,18 @@
+-- LeetCode #1075
+-- Project Employees I
+
+-- Problem:
+-- Find the average experience years of employees
+-- working on each project.
+
+SELECT
+    p.project_id,
+    ROUND(
+        SUM(e.experience_years) / COUNT(e.employee_id),
+        2
+    ) AS average_years
+FROM Project AS p
+LEFT JOIN Employee AS e
+    ON p.employee_id = e.employee_id
+GROUP BY
+    p.project_id;
